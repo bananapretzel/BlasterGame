c1825b5329795f01b527bf3dcf1a35fd6a95cab3
Kccot <cotca309@student.otago.ac.nz>
2023-10-21 17:39:33 +1300

    Removing untracked folders/files

--- Source/Blaster/BlasterTypes/CombatState.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-UENUM(BlueprintType)
-enum class ECombatState : uint8 {
-	ECS_Unoccupied UMETA(DisplayName = "Unoccupied"),
-	ECS_Reloading UMETA(DisplayName = "Reloading"),
-	ECS_ThrowingGrenade UMETA(DisplayName = "Throwing Grenade"),
-	ECS_MAX UMETA(DisplayName = "DefaultMAX")
-};
