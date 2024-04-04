4ec3eda02f2cbc8c7391101746e8474f295a1c3b
Kccot <cotca309@student.otago.ac.nz>
2024-04-05 00:33:00 +1300

    Added a splash screen after a teams match to show which team won or lost.

---
+++ Source/Blaster/BlasterTypes/Announcement.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Announcement {
+	const FString NewMatchStartsIn(TEXT("NEW MATCH STARTS IN:"));
+	const FString ThereIsNoWinner(TEXT("There is no winner"));
+	const FString YouAreTheWinner(TEXT("You are the winner!"));
+	const FString PlayersTiedForTheWin(TEXT("Players tied for the win:\n"));
+	const FString TeamsTiedForTheWin(TEXT("Teams Tied For The Win:"));
+	const FString RedTeam(TEXT("Red Team"));
+	const FString BlueTeam(TEXT("Blue Team"));
+	const FString RedTeamWins(TEXT("Red Team wins!"));
+	const FString BlueTeamWins(TEXT("Blue Team wins!"));
+}
